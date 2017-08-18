7c6321f25f35c60c1684907364fd341d4e7dfb14
Vinoth <vin10catchme@gmail.com>
2017-08-19 00:28:30 +0530

    https://www.hackerrank.com/contests/gs-codesprint/challenges/buy-maximum-stocks initial commit

---
+++ Java/src/contests/goldmansachs/MaximumStocks.java
@@ -0,0 +1,40 @@
+package contests.goldmansachs;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class MaximumStocks {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+		int temp =0;
+		for(int i=1;i<=n;i++){
+			temp = in.nextInt();
+			map.put(temp, i);
+		}
+		int k = in.nextInt(), sum=0;
+//		System.out.println("->"+map);
+//		System.out.println("k:"+k);
+		for(int stockPrice : map.keySet()){
+			if(stockPrice<=k){
+				int maxStocks=map.get(stockPrice);
+				int t=k/stockPrice;
+				if(t<maxStocks){
+					sum+=t;
+					k=k-(t*stockPrice);
+				}else{
+					sum+=maxStocks;
+					k=k-(maxStocks*stockPrice);
+				}
+//				sum+=t;
+//				k=k-t;
+				//System.out.println("price"+stockPrice+" maxstocks:"+maxStocks+"  k:"+k);
+			}else{
+				break;
+			}
+		}
+		System.out.println(sum);
+		in.close();
+	}
+}
