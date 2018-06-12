dbde0c3a4ab9d26b1f8cddb7c8f4766140eb0a01
Vinoth G <vin10catchme@gmail.com>
2018-06-13 00:01:08 +0530

    "Last commit with Pending test case"

--- python/leetcode/1TwoSum.py
+++ python/leetcode/1TwoSum.py
@@ -4,17 +4,31 @@ from re import search
 
 def twoSum(nums, target):
     map = dict()
+    map1 = dict()
     for i in range(0, len(nums)):
-       map[nums[i]] = i
-    print(map)
+        if nums[i] in map:
+            map1[nums[i]] = i
+        else:
+            map[nums[i]] = i
+    # print(map)
+
     for i in range(0, len(nums)):
         # print(nums[i])
-        del map[nums[i]]
+        a=0;
+        if nums[i] in map1:
+            del map1[nums[i]]
+            a=0
+        else:
+            a=1
+            del map[nums[i]]
         # print("->", target-nums[i])
         if (target-nums[i]) in map:
             # print("exist")
             a = map[target - nums[i]]
             # print(i, " ", map[target-nums[i]])
             return [i, a]
-        map[nums[i]] = i
+        if a==0:
+            map1[nums[i]] = i
+        else:
+            map[nums[i]] = i
 print(twoSum([2, 7, 11, 15], 9 ))
