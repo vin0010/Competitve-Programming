37625830f000ac34acf06f882905d74d61996eb5
Vinoth Gopu <vinoth.gopu@asg.com>
2018-06-19 10:38:04 +0530

    "Updated code"

--- python/leetcode/1TwoSum.py
+++ python/leetcode/1TwoSum.py
@@ -1,34 +1,23 @@
-#https://leetcode.com/problems/two-sum/description/
+# https://leetcode.com/problems/two-sum/description/
 from re import search
 
 
 def twoSum(nums, target):
     map = dict()
-    map1 = dict()
     for i in range(0, len(nums)):
-        if nums[i] in map:
-            map1[nums[i]] = i
-        else:
-            map[nums[i]] = i
-    # print(map)
+        tup = map.setdefault(nums[i], (0, []))
+        x = tup[0]+1
+        tup[1].append(i)
+        map[nums[i]] = (x, tup[1])
+    for i in map.keys():
+        print(i, map[i])
+    for i in range(0, len(nums)):
+        #remove from map
+        tup = map[nums[i]]
+        print("--->",tup)
+        print(tup[0]-1, tup[1].pop(0))
+        #new_tup = (tup[0]-1, tup[1])
 
-    for i in range(0, len(nums)):
-        # print(nums[i])
-        a=0;
-        if nums[i] in map1:
-            del map1[nums[i]]
-            a=0
-        else:
-            a=1
-            del map[nums[i]]
-        # print("->", target-nums[i])
-        if (target-nums[i]) in map:
-            # print("exist")
-            a = map[target - nums[i]]
-            # print(i, " ", map[target-nums[i]])
-            return [i, a]
-        if a==0:
-            map1[nums[i]] = i
-        else:
-            map[nums[i]] = i
-print(twoSum([2, 7, 11, 15], 9 ))
+
+#print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 3], 9))
