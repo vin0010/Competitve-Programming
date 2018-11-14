beada5386ed596b6f6150b9194ff878381440be0
Vinoth G <vin10catchme@gmail.com>
2018-11-14 16:15:38 +0530

    logic update

--- python/leetcode/add_two_numbers.py
+++ python/leetcode/add_two_numbers.py
@@ -22,17 +22,44 @@
     ------
     987444
 
+193
+329
+---
+
+
+
 """
 from hmac import new
 
 
-def add_two_numbers(self, l1, l2):
+def add_two_numbers(l1, l2):
     """
     :type l1: ListNode
     :type l2: ListNode
     :rtype: ListNode
     """
-    
+    result = i = ListNode(0)
+    reminder = 0
+    while l1 or l2:
+        if l1 and l2:
+            total = l1.val + l2.val + reminder
+            print("-->", total)
+            node = ListNode(total % 10)
+            i.next = node
+            reminder = 0 if total < 10 else total//10
+            l1 = l1.next
+            l2 = l2.next
+            i = i.next
+    while l1: #l1 is not empty
+        total = l1.val + reminder
+        node = ListNode(total % 10)
+        i.next = node
+        
+
+    print(reminder)
+    return result.next
+
+
 
 def print_list_node(l1):
     while l1:
@@ -48,14 +75,20 @@ class ListNode:
 
 
 print("-----")
-node1 = ListNode(2)
-node2 = ListNode(4)
-node3 = ListNode(3)
+node1 = ListNode(8)
+node2 = ListNode(7)
+node3 = ListNode(9)
 node1.next = node2
 node2.next = node3
 
-n1 = ListNode(5)
-n2 = ListNode(6)
-n3 = ListNode(4)
+n1 = ListNode(7)
+n2 = ListNode(3)
+n3 = ListNode(3)
 n1.next = n2
 n2.next = n3
+
+print_list_node(node1)
+print()
+print_list_node(n1)
+print()
+print_list_node(add_two_numbers(node1, n1))
