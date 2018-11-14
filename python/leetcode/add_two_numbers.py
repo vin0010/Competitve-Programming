869ce4694edaea759bf6f7e60efe0b40c0099856
Vinoth G <vin10catchme@gmail.com>
2018-11-14 17:13:31 +0530

    working logic

--- python/leetcode/add_two_numbers.py
+++ python/leetcode/add_two_numbers.py
@@ -5,6 +5,8 @@
         different length - since they are in reverse order dont worry about starting from first
         l1 is big
         l2 is big
+    missed edge cases
+
 
     (2 -> 4 -> 3) + (5 -> 6 -> 4) = 7 -> 0 -> 8
     342 + 465 = 807
@@ -40,25 +42,37 @@ def add_two_numbers(l1, l2):
     """
     result = i = ListNode(0)
     reminder = 0
-    while l1 or l2:
-        if l1 and l2:
-            total = l1.val + l2.val + reminder
-            print("-->", total)
-            node = ListNode(total % 10)
-            i.next = node
-            reminder = 0 if total < 10 else total//10
-            l1 = l1.next
-            l2 = l2.next
-            i = i.next
-    while l1: #l1 is not empty
-        total = l1.val + reminder
+    while l1 and l2:
+        total = l1.val + l2.val + reminder
+        # print("-->>", total)
         node = ListNode(total % 10)
         i.next = node
-        
+        reminder = 0 if total < 10 else total // 10
+        print("reminder:", reminder)
+        l1 = l1.next
+        l2 = l2.next
+        i = i.next
+    print_list_node(result.next)
+    print()
+    while l1:  # l1 is not empty
+        total = l1.val + reminder
+        print("total:", total)
+        node = ListNode(total % 10)
+        i.next = node
+        reminder = 0 if total < 10 else total // 10
+        i = i.next
+        l1 = l1.next
 
+    while l2:  # l2 is not empty
+        total = l2.val + reminder
+        node = ListNode(total % 10)
+        i.next = node
+        reminder = 0 if total < 10 else total // 10
+        l2 = l2.next
     print(reminder)
+    if reminder > 0:
+        i.next = ListNode(reminder)
     return result.next
-
 
 
 def print_list_node(l1):
@@ -75,17 +89,20 @@ class ListNode:
 
 
 print("-----")
-node1 = ListNode(8)
-node2 = ListNode(7)
+node1 = ListNode(9)
+node2 = ListNode(9)
 node3 = ListNode(9)
+node4 = ListNode(4)
+
 node1.next = node2
 node2.next = node3
+node3.next = node4
 
-n1 = ListNode(7)
-n2 = ListNode(3)
-n3 = ListNode(3)
+n1 = ListNode(2)
+n2 = ListNode(1)
+# n3 = ListNode(4)
 n1.next = n2
-n2.next = n3
+# n2.next = n3
 
 print_list_node(node1)
 print()
