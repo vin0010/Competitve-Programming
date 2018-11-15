bfa9d8f5a25aad6db3aaada21aa43669a7256a1a
Vinoth G <vin10catchme@gmail.com>
2018-11-15 12:39:19 +0530

    improved solution initial commit for median_of_two_sorted_arrays

---
+++ python/leetcode/median_of_two_sorted_arrays_best.py
@@ -0,0 +1,12 @@
+
+"""
+    https://leetcode.com/problems/median-of-two-sorted-arrays/
+    Problem:
+        Find median of two sorted arrays
+    Edge cases
+
+    Solution
+        if two arrays not overlaps, then mid element is median(mid + next mid in case of array with even number of cells)
+        if overlap, there are 4 possibilities
+
+"""
