5d600f40030a7ed099c586cda2d7ed9e9c2578de
Vinoth G <vin10catchme@gmail.com>
2018-11-15 22:02:41 +0530

    update

--- python/leetcode/merge_k_sorted_lists.py
+++ python/leetcode/merge_k_sorted_lists.py
@@ -1,7 +1,21 @@
 """
     https://leetcode.com/problems/merge-k-sorted-lists/
-    Problem : merge k sorted lists
+    Problem : merge k sorted linked lists
     Edge cases:
-        
+
     Solution
 """
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def merge_k_lists(lists):
+    """
+    :type lists: List[ListNode]
+    :rtype: ListNode
+    """
+    for list in lists:
+        print("started")
