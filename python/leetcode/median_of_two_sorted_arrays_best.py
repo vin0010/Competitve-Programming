d475adc62c4f407ebcf1948b5300ae8cf5488cd8
Vinoth G <vin10catchme@gmail.com>
2018-11-15 13:52:26 +0530

    updated best solution approach

--- python/leetcode/median_of_two_sorted_arrays_best.py
+++ python/leetcode/median_of_two_sorted_arrays_best.py
@@ -8,5 +8,17 @@
     Solution
         if two arrays not overlaps, then mid element is median(mid + next mid in case of array with even number of cells)
         if overlap, there are 4 possibilities
+        1.      _______________
+                    ______
+            compare start1 with start2 
+
+        2.          ______
+                _______________
+
+        3.      ________________
+                    ________________
+
+        4.          ________________
+                ________________
 
 """
