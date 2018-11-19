d91832a1adc6b75fe6af3775d6ee70e0de38503d
Vinoth G <vin10catchme@gmail.com>
2018-11-20 01:02:10 +0530

    https://leetcode.com/problems/trapping-rain-water/ initial commit

---
+++ python/leetcode/trapping_rain_water.py
@@ -0,0 +1,16 @@
+"""
+    https://leetcode.com/problems/trapping-rain-water/
+    https://www.geeksforgeeks.org/trapping-rain-water/
+
+    edge cases:
+        0, 0, 0, 1, 2, 3, 2, 1, 0
+
+    Sollution:
+    Input: arr[] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    Output: 6
+    XXXXXXXXXX|
+    XXX|XXX||X|
+    _|_||_||||||
+
+
+"""
