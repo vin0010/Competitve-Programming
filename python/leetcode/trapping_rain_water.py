3681c532ed32c8fa3e6ec26f72e51f56e8e3af81
Vinoth G <vin10catchme@gmail.com>
2018-11-21 00:13:01 +0530

    Solution approach analysis for trapping rain water problem

--- python/leetcode/trapping_rain_water.py
+++ python/leetcode/trapping_rain_water.py
@@ -9,10 +9,22 @@
         5, 0, 4, 0, 3, 0, 2, 0, 0
 
     Sollution:
+        For any bar find biggest on the left and biggest on the right - how can you find solution from this point?
+
+        I have to find
+            How to find the trapped water.
+                If you find subsequent increasing number
+
+        Is it something related to kadane's algorithm?
+        Longest sum subarray
+
+
+            If I am not sure about the trapped length, how to adjust it.
     Input: arr[] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
     Output: 6
     XXXXXXXXXX|
     XXX|XXX||X|
     _|_||_||||||
+
 
 """
