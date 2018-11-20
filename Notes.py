2289fd5ca092fe96071cf16e331c83656121b543
Vinoth G <vin10catchme@gmail.com>
2018-11-20 18:37:27 +0530

    updates about asymptotic analysis

--- Notes.py
+++ Notes.py
@@ -1,4 +1,10 @@
 Big O notation
     O refers to the order of the function
     n refers to the length of array to be sorted.
-    
+    f(n) = 6n^4 - 2n^3 + 5
+        n^4 is having higher significant rate. we can ignore n^3 since it has less significance in this function. As n reaches infinity, only n^4 does matters.
+
+    O(n log n) - Quick, Heap, Merge,
+    https://cdn-images-1.medium.com/max/883/1*KfZYFUT2OKfjekJlCeYvuQ.jpeg
+    Most language's in built sorting is based on QuickSort.
+    if you are trying to prepare for your first interview, or perhaps you struggled in your last one, increasing your knowledge on concepts like Big O Notation and other computer science topics will help give you a leg up. 
