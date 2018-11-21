5fd1aca69156eb3d93b1a00d2cb4a618c87d9397
Vinoth G <vin10catchme@gmail.com>
2018-11-21 23:04:52 +0530

    added python language notes for my reference

---
+++ python.py
@@ -0,0 +1,10 @@
+Python
+    
+    
+    List
+        Swap list entries in python
+        i[b], i[a] = i[a], i[b]
+
+    Map
+        create map in different ways
+        https://www.geeksforgeeks.org/python-map-function/
