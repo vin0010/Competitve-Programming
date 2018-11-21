21f7bbbc565c58a11fde88d72b432fd58066f2f6
Vinoth G <vin10catchme@gmail.com>
2018-11-22 01:23:48 +0530

    Initial commit for https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/

---
+++ python/leetcode/subarray_with_sum_k.py
@@ -0,0 +1,8 @@
+"""
+    https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+    Problem
+        Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K.
+        If there is no non-empty subarray with sum at least K, return -1.
+    Solution
+        Bruteforce not going to help
+"""
