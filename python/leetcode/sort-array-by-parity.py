5f867924b3f6f017ea0f8019c92ebda5fc6be79d
Vinoth G <vin10catchme@gmail.com>
2018-11-21 22:46:10 +0530

    initial commit for https://leetcode.com/problems/sort-array-by-parity/

---
+++ python/leetcode/sort-array-by-parity.py
@@ -0,0 +1,8 @@
+"""
+    https://leetcode.com/problems/sort-array-by-parity/
+    Problem:
+        Move even to left and odd to right
+    Solution:
+        in a while loop, set left and right pointer and swap or move until both becomes same
+"""
+
