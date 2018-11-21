54a43a30f4d00f6d832ebfd88a9182fee9e660b9
Vinoth G <vin10catchme@gmail.com>
2018-11-21 23:17:25 +0530

    final solution for https://leetcode.com/problems/sort-array-by-parity/

--- python/leetcode/sort-array-by-parity.py
+++ python/leetcode/sort-array-by-parity.py
@@ -6,3 +6,30 @@
         in a while loop, set left and right pointer and swap or move until both becomes same
 """
 
+
+def sort_array_by_parity(A):
+    """
+    :type A: List[int]
+    :rtype: List[int]
+    """
+    i = 0
+    j = len(A) - 1
+
+    while i < j and i < len(A) and j > -1:
+        a = A[i] % 2 == 0
+        b = A[j] % 2 == 1
+        if a and b:
+            i += 1
+            j -= 1
+        elif a:
+            i += 1
+        elif b:
+            j -= 1
+        else:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+            j -= 1
+    return A
+
+
+sort_array_by_parity([3, 1, 2, 4])
