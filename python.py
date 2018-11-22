8944d77c7cf94815426162d17c414eed862af718
Vinoth G <vin10catchme@gmail.com>
2018-11-22 17:46:06 +0530

    updated list tricks

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
 Python
-    
-    
+
+
     List
         Swap list entries in python
         i[b], i[a] = i[a], i[b]
+        List with values
+            b=[None]*100 -> will initialize list with None for 100 cells
 
     Map
         create map in different ways
