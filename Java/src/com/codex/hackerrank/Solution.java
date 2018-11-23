19095ebc30cd06e64b334274b31ac6d01bc902a3
Vinoth G <vin10catchme@gmail.com>
2018-11-24 02:19:37 +0530

    updated solution with minor bugs

--- Java/src/com/codex/hackerrank/Solution.java
+++ Java/src/com/codex/hackerrank/Solution.java
@@ -1,5 +1,6 @@
 package com.codex.hackerrank;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -22,31 +23,37 @@ import java.util.Scanner;
  * 
  */
 
-public class MedianUpdates {
-	class MyComparator implements Comparator<Integer> {
-
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
-		}
-	}
+public class Solution {
+//	class MyComparator implements Comparator<Integer> { // commented to solve hackerrank behavior
+//
+//		@Override
+//		public int compare(Integer o1, Integer o2) {
+//			return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
+//		}
+//	}
 
 	private static void printMedian(PriorityQueue<Integer> leftHeap, PriorityQueue<Integer> rightHeap) {
 		int l = leftHeap.size();
 		int r = rightHeap.size();
-//		System.out.println(l + "----" + r);
+//		System.out.println(l + "-----------" + r);
 		if (l == 0 && r == 0) {
 			System.out.println("Wrong!");
 		} else {
-			if ((l + r) % 2 == 0) {
-				System.out.println(leftHeap.peek() + "--------" + rightHeap.peek());
+			if (l == r) {
+				Long sum = (long) leftHeap.peek() + (long)rightHeap.peek();
+//				System.out.println("Sum:" + sum);
+				if (sum % 2 == 0) {
+					System.out.println(sum / 2);
+				} else {
+					System.out.println((float) sum / 2);
+				}
 			} else if (l > r) {
 				System.out.println(leftHeap.peek());
 			} else {
 				System.out.println(rightHeap.peek());
 			}
 		}
-		
+
 	}
 
 	private static void balanceHeap(PriorityQueue<Integer> leftHeap, PriorityQueue<Integer> rightHeap) {
@@ -67,10 +74,16 @@ public class MedianUpdates {
 	}
 
 	private static void median(int[] numbers, String[] operations) {
-		PriorityQueue<Integer> leftHeap = new PriorityQueue<>();
-		MedianUpdates medianUpdates = new MedianUpdates();
-		Comparator<Integer> comparator = medianUpdates.new MyComparator();
-		PriorityQueue<Integer> rightHeap = new PriorityQueue<>(comparator);
+		PriorityQueue<Integer> leftHeap = new PriorityQueue<Integer>();
+		Solution medianUpdates = new Solution();
+//		Comparator<Integer> comparator = medianUpdates.new MyComparator();
+		PriorityQueue<Integer> rightHeap = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
+			}
+		});
+//		PriorityQueue<Integer> rightHeap = new PriorityQueue<Integer>(comparator);
 		for (int i = 0; i < numbers.length; i++) {
 			if (operations[i].equals("r")) {
 				if (!leftHeap.remove(numbers[i])) {
@@ -85,37 +98,7 @@ public class MedianUpdates {
 	}
 
 	public static void main(String[] args) {
-//		checkBalacnce();
-//		System.exit(0);
-		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-		queue.add(10);
-		queue.add(13);
-		queue.add(11);
-		queue.add(19);
-		queue.add(100);
-		queue.add(90);
-//		System.out.println("------>" + queue.peek());
-//		while (!queue.isEmpty()) {
-//			System.out.println(queue.poll());
-//		}
-		MedianUpdates medianUpdates = new MedianUpdates();
-		Comparator<Integer> comparator = medianUpdates.new MyComparator();
-
-//		System.out.println("----------------");
-		PriorityQueue<Integer> queue1 = new PriorityQueue<Integer>(comparator);
-		queue1.add(10);
-		queue1.add(11);
-		queue1.add(3);
-		queue1.add(4);
-		queue1.add(7);
-		queue1.add(2);
-		queue1.add(14);
-//		System.out.println("---->" + queue1.peek());
-//		while (!queue1.isEmpty()) {
-//			System.out.println(queue1.poll());
-//		}
 		Scanner in = new Scanner(System.in);
-
 		int N;
 		N = in.nextInt();
 
@@ -125,42 +108,8 @@ public class MedianUpdates {
 		for (int i = 0; i < N; i++) {
 			s[i] = in.next();
 			x[i] = in.nextInt();
-//			System.out.println("-->" + s[i] + ":" + x[i]);
 		}
 		median(x, s);
 		in.close();
-
-	}
-
-	private static void checkBalacnce() {
-		// TODO Auto-generated method stub
-		MedianUpdates medianUpdates = new MedianUpdates();
-		Comparator<Integer> comparator = medianUpdates.new MyComparator();
-		PriorityQueue<Integer> queue2 = new PriorityQueue<>();
-		PriorityQueue<Integer> queue1 = new PriorityQueue<Integer>(comparator);
-		queue2.add(10);
-		queue2.add(13);
-		queue2.add(17);
-		queue2.add(19);
-		queue2.add(100);
-		queue2.add(90);
-		queue2.add(90);
-		System.out.println(queue2);
-		System.out.println(queue2.remove(200));
-		System.out.println(queue2);
-
-//		queue1.add(10);
-//		queue1.add(11);
-//		queue1.add(3);
-//		queue1.add(4);
-//		queue1.add(7);
-//		queue1.add(2);
-//		queue1.add(14);
-		balanceHeap(queue1, queue2);
-		System.out.println("Queue1:--->" + queue1);
-		System.out.println("Queue2:--->" + queue2);
-//		while (!queue2.isEmpty()) {
-//			System.out.println(queue2.poll());
-//		}
 	}
 }
