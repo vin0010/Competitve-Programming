5e42601c756afd74ff3c005e67c8395965e8cf6d
Vinoth G <vin10catchme@gmail.com>
2018-11-24 14:44:15 +0530

    Almost final solution

--- Java/src/com/codex/hackerrank/Solution.java
+++ Java/src/com/codex/hackerrank/Solution.java
@@ -1,52 +1,57 @@
 package com.codex.hackerrank;
 
+import java.text.DecimalFormat;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 /*
- * 	Problem : https://www.hackerrank.com/challenges/median/problem
- * 	Solution:
- * 			Key point is we don't have to sort the items every time since we just want the median.
- * 			
- * 			Keep left heap(max heap) and right heap(min heap).  
- * 			Center element required? - for starters I dont keep center element
- * 			
- * 				7  
-				r 1  
-				a 1  
-				a 2  
-				a 1  
-				r 1  
-				r 2  
-				r 1  
+ *     Problem : https://www.hackerrank.com/challenges/median/problem
+ *     Solution:
+ *             Key point is we don't have to sort the items every time since we just want the median.
+ *             
+ *             Keep left heap(max heap) and right heap(min heap).  
+ *             Center element required? - for starters I dont keep center element
+ *             
+ *                 7  
+                r 1  
+                a 1  
+                a 2  
+                a 1  
+                r 1  
+                r 2  
+                r 1  
  * 
  */
 
 public class Solution {
-//	class MyComparator implements Comparator<Integer> { // commented to solve hackerrank behavior
-//
-//		@Override
-//		public int compare(Integer o1, Integer o2) {
-//			return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
-//		}
-//	}
+	// class MyComparator implements Comparator<Integer> { // commented to solve
+	// hackerrank behavior
+	//
+	// @Override
+	// public int compare(Integer o1, Integer o2) {
+	// return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
+	// }
+	// }
 
-	private static void printMedian(PriorityQueue<Integer> leftHeap, PriorityQueue<Integer> rightHeap) {
-		System.out.println("Left:"+leftHeap);
-		System.out.println("Right:"+rightHeap);
+	private static void printMedian(PriorityQueue<Integer> leftHeap, PriorityQueue<Integer> rightHeap,
+			boolean isNotRemoved) {
+		System.out.println("Left:" + leftHeap);
+		System.out.println("Right:" + rightHeap);
 		int l = leftHeap.size();
 		int r = rightHeap.size();
 //		System.out.println(l + "-----------" + r);
-		if (l == 0 && r == 0) {
+		if (l == 0 && r == 0 || isNotRemoved) {
 			System.out.println("Wrong!");
 		} else {
 			if (l == r) {
-				Long sum = (long) leftHeap.peek() + (long)rightHeap.peek();
-//				System.out.println("Sum:" + sum);
+				Long sum = (long) leftHeap.peek() + (long) rightHeap.peek();
+				// System.out.println("Sum:" + sum);
 				if (sum % 2 == 0) {
 					System.out.println(sum / 2);
 				} else {
-					System.out.println((float) sum / 2);
+					DecimalFormat df = new DecimalFormat("##############.##########");
+					float res = sum / 2;
+					System.out.println("--->"+df.format(res));
 				}
 			} else if (l > r) {
 				System.out.println(leftHeap.peek());
@@ -76,25 +81,26 @@ public class Solution {
 
 	private static void median(int[] numbers, String[] operations) {
 		PriorityQueue<Integer> leftHeap = new PriorityQueue<Integer>();
-		Solution medianUpdates = new Solution();
-//		Comparator<Integer> comparator = medianUpdates.new MyComparator();
-		PriorityQueue<Integer> rightHeap = new PriorityQueue<>(new Comparator<Integer>() {
+		PriorityQueue<Integer> rightHeap = new PriorityQueue<Integer>(100, new Comparator<Integer>() {
 			@Override
 			public int compare(Integer o1, Integer o2) {
 				return o1 > o2 ? -1 : (o1 == o2 ? 0 : 1);
 			}
 		});
-//		PriorityQueue<Integer> rightHeap = new PriorityQueue<Integer>(comparator);
+		// PriorityQueue<Integer> rightHeap = new PriorityQueue<Integer>(comparator);
 		for (int i = 0; i < numbers.length; i++) {
+			boolean isNotRemoved = false;
 			if (operations[i].equals("r")) {
 				if (!leftHeap.remove(numbers[i])) {
-					rightHeap.remove(numbers[i]);
+					if (!rightHeap.remove(numbers[i]))
+						isNotRemoved = true;
 				}
 			} else if (operations[i].equals("a")) {
 				leftHeap.add(numbers[i]);
 			}
 			balanceHeap(leftHeap, rightHeap);
-			printMedian(leftHeap, rightHeap);
+			System.out.println("---------------------\nAfter :" + operations[i] + " " + numbers[i]);
+			printMedian(leftHeap, rightHeap, isNotRemoved);
 		}
 	}
 
@@ -114,36 +120,9 @@ public class Solution {
 		in.close();
 	}
 	/*
-	 15
-a -2147483648
-a -2147483648
-a -2147483647
-r -2147483648
-a 2147483647
-r -2147483648
-a 10
-a 10
-a 10
-r 10
-r 10
-r 10
-r 100
-r 100
-r 100
--2147483648
--2147483648
--2147483648
--2.14748365E9
--2147483647
-0
-10
-10
-10
-10
-10
-0
-0
-0
-0
+	 * 15 a -2147483648 a -2147483648 a -2147483647 r -2147483648 a 2147483647 r
+	 * -2147483648 a 10 a 10 a 10 r 10 r 10 r 10 r 100 r 100 r 100 -2147483648
+	 * -2147483648 -2147483648 -2.14748365E9 -2147483647 0 10 10 10 10 10 0 0 0 0
 	 */
+
 }
