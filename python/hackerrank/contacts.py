dd879f27299612c2a4b69a86746cba935b7fe75d
Vinoth G <vin10catchme@gmail.com>
2018-11-28 17:55:57 +0530

    updated solution

--- python/hackerrank/contacts.py
+++ python/hackerrank/contacts.py
@@ -3,3 +3,26 @@
     Problem:
         contact add and search operation
 """
+
+
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+def create_trie(names):
+    trie = dict()
+    for name in names:
+        current_dict = trie
+        for char in name:
+            if char in current_dict:
+                tuple = current_dict[char]
+            else:
+                tuple = (1, dict())
+            current_dict = current_dict.setdefault(char, {})
+    return trie
+
+
+print(create_trie(['add', 'ads', 'act']))
