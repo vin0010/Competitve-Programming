98b641a86a0e4a9c91394b75d1b6dc313a129c66
Vinoth G <vin10catchme@gmail.com>
2018-11-28 21:46:19 +0530

    final solution for conatcts' '

--- python/hackerrank/contacts.py
+++ python/hackerrank/contacts.py
@@ -4,6 +4,15 @@
         contact add and search operation
 """
 
+# !/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the contacts function below.
+#
 def find(trie, z):
     res = 0
     current_dict = trie
@@ -11,13 +20,26 @@ def find(trie, z):
         if i in current_dict:
             res = current_dict[i][0]
             current_dict = current_dict[i][1]
-        else :
+        else:
             return 0
     return res
 
 
-def create_trie(names):
-    trie = dict()
+def update_trie(trie, name):
+    current_dict = trie
+    for char in name:
+        if char in current_dict:
+            tup = current_dict[char]  # TODO
+            new_tup = (tup[0] + 1, tup[1])
+            current_dict[char] = new_tup
+        else:
+            tup = (1, dict())
+            current_dict[char] = tup
+        current_dict = current_dict[char][1]
+    return trie
+
+
+def create_trie(trie, names):
     for name in names:
         current_dict = trie
         for char in name:
@@ -31,8 +53,31 @@ def create_trie(names):
             current_dict = current_dict[char][1]
     return trie
 
-result = create_trie(['add', 'bad', 'adder', 'ads'])
-print(result)
-# print(create_trie(['add', 'ads', 'act', 'bad']))
-# print(find(result, 'add'))
-print(find(result, 'ad'))
+
+def contacts(queries):
+    my_trie = dict()
+    # print(queries)
+    for x in queries:
+        if x[0]=='add':
+            my_trie = update_trie(my_trie, x[1])
+        else :
+            print(find(my_trie, x[1]))
+    #
+    # Write your code here.
+    #
+
+# contacts([['add', 'hack'], ['add', 'hackerrank'], ['find', 'hac'], ['find', 'hak']])
+
+queries_rows = int(input())
+queries = []
+
+for _ in range(queries_rows):
+    queries.append(input().rstrip().split())
+
+contacts(queries)
+
+# result = create_trie(['ask', 'add', 'bad', 'adder', 'ads'])
+# print(result)
+# # print(create_trie(['add', 'ads', 'act', 'bad']))
+# # print(find(result, 'add'))
+# print(find(result, 'a'))
