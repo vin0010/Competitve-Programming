fb7a4e2669685e0105c56b09ac0ff0c88f91812c
Vinoth G <vin10catchme@gmail.com>
2018-11-28 19:02:20 +0530

    final solution with static input

--- python/hackerrank/contacts.py
+++ python/hackerrank/contacts.py
@@ -4,12 +4,16 @@
         contact add and search operation
 """
 
-
-class Node:
-    def __init__(self, val):
-        self.left = None
-        self.right = None
-        self.val = val
+def find(trie, z):
+    res = 0
+    current_dict = trie
+    for i in z:
+        if i in current_dict:
+            res = current_dict[i][0]
+            current_dict = current_dict[i][1]
+        else :
+            return 0
+    return res
 
 
 def create_trie(names):
@@ -18,11 +22,17 @@ def create_trie(names):
         current_dict = trie
         for char in name:
             if char in current_dict:
-                tuple = current_dict[char]
+                tup = current_dict[char]  # TODO
+                new_tup = (tup[0] + 1, tup[1])
+                current_dict[char] = new_tup
             else:
-                tuple = (1, dict())
-            current_dict = current_dict.setdefault(char, {})
+                tup = (1, dict())
+                current_dict[char] = tup
+            current_dict = current_dict[char][1]
     return trie
 
-
-print(create_trie(['add', 'ads', 'act']))
+result = create_trie(['add', 'bad', 'adder', 'ads'])
+print(result)
+# print(create_trie(['add', 'ads', 'act', 'bad']))
+# print(find(result, 'add'))
+print(find(result, 'ad'))
