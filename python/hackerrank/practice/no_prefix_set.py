7648d998408bc379df4d72f13ca090703f3491b0
Vinoth G <vin10catchme@gmail.com>
2018-11-29 01:03:05 +0530

    accepted solution

--- python/hackerrank/practice/no_prefix_set.py
+++ python/hackerrank/practice/no_prefix_set.py
@@ -1,5 +1,13 @@
 """
     https://www.hackerrank.com/challenges/no-prefix-set/problem
+7
+aab
+defgab
+abcde
+abc
+cedaaa
+bbbbbbbbbb
+jabjjjad
 
 """
 
@@ -10,17 +18,29 @@ def update_trie(names):
     # current_dict = trie
     for name in names:
         current_dict = trie
+        flag = True
         for i in range(len(name)):
-            current_dict = current_dict.setdefault(name[i], dict())
+            if name[i] in current_dict:
+                current_dict = current_dict[name[i]]
+            else:
+                flag = False
+                current_dict[name[i]] = dict()
+                current_dict = current_dict[name[i]]
+            # current_dict = current_dict.setdefault(name[i], dict())
+
             if _end_ in current_dict:
-                print(current_dict)
+                # print(current_dict)
                 print('BAD SET')
                 print(name)
                 return
+        if flag:
+            print('BAD SET')
+            print(name)
+            return
         current_dict[_end_] = _end_
-
+    # print(trie)
     print('GOOD SET')
-            # print(name[:i+1]) if _end_ in current_dict else 0
+    # print(name[:i+1]) if _end_ in current_dict else 0
 
 
 my_trie = dict()
