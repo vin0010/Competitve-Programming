bed02bb54f0d98856859f6748576aec6699e3e2f
Vinoth G <vin10catchme@gmail.com>
2018-11-29 10:50:16 +0530

    initial commit for https://www.hackerrank.com/challenges/find-maximum-index-product/problem

---
+++ python/hackerrank/practice/find_maximum_index_product.py
@@ -0,0 +1,7 @@
+"""
+    https://www.hackerrank.com/challenges/find-maximum-index-product/problem
+    5
+    5 4 3 4 5
+    Solution:
+    find any index i find closest index j on the left side such that 
+"""
