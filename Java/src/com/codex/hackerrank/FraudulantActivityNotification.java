d0b4e48508248bf9891d71816fd8c4cf83bda141
Vinoth G <vin10catchme@gmail.com>
2018-12-03 12:12:29 +0530

    initial commit

---
+++ Java/src/com/codex/hackerrank/FraudulantActivityNotification.java
@@ -0,0 +1,17 @@
+package com.codex.hackerrank;
+
+import java.util.Scanner;
+
+public class FraudulantActivityNotification {
+	static int activityNotifications(int[] expenditure, int d) {
+
+		return 0;
+    }
+	
+	public static void main(String[] args) {
+		try(Scanner scanner = new Scanner(System.in)){
+			int n = scanner.nextInt();
+			
+		}
+	}
+}
