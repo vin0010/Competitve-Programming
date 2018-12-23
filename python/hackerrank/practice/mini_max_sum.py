7abf3e8d59a2017c2678b2c64e305335e062afb2
Vinoth G <vin10catchme@gmail.com>
2018-12-23 17:33:54 +0530

    Initial commit for https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ python/hackerrank/practice/mini_max_sum.py
@@ -0,0 +1,9 @@
+"""
+    https://www.hackerrank.com/challenges/mini-max-sum/problem
+"""
+arr = list(map(int, input().split()))
+print(arr)
+
+min = arr[0], max = arr[0]
+
+sum(sum(arr[0:len(arr)]))
