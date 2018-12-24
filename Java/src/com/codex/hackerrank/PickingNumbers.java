84c23136a551964a850680dd4a10ee37d98115e7
Vinoth G <vin10catchme@gmail.com>
2018-12-24 11:04:37 +0530

    updated code

--- Java/src/com/codex/hackerrank/PickingNumbers.java
+++ Java/src/com/codex/hackerrank/PickingNumbers.java
@@ -1,17 +1,43 @@
 package com.codex.hackerrank;
+
 /*
  * https://www.hackerrank.com/challenges/picking-numbers/problem
  */
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class PickingNumbers {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		int[] arr = new int[n];
+		TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+
 		for (int t = 0; t < n; t++) {
-			arr[i] = scanner.nextInt();
+			int x = scanner.nextInt();
+			if (map.containsKey(x)) {
+				map.put(x, map.get(x) + 1);
+			} else {
+				map.put(x, 1);
+			}
 		}
+		Integer[] keys = (Integer[]) map.keySet().toArray();
+		int max = map.get(keys[0]);
+		int currentMax = max;
+		for (int i = 1; i < keys.length; i++) {
+			if (Math.abs(keys[i] - keys[i + 1]) == 1) {
+				currentMax += map.get(keys[i + 1]);
+			} else {
+				currentMax = map.get(keys[i]);
+			}
+			max = max(currentMax, max);
+		}
+
+		System.out.println(map);
+		System.out.println(max);
 		scanner.close();
+	}
+
+	private static int max(int a, int b) {
+		return a > b ? a : b;
 	}
 }
