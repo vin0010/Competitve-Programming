3a2d5ce01922db58a7228dd4c2733c6f3c6b6a90
Vinoth G <vin10catchme@gmail.com>
2018-12-24 10:28:32 +0530

    paused

--- python/hackerrank/practice/picking_numbers.py
+++ python/hackerrank/practice/picking_numbers.py
@@ -1,3 +1,16 @@
 """
 https://www.hackerrank.com/challenges/picking-numbers/problem
 """
+n = int(input())
+arr = list(map(int, input().split()))
+
+my_dict = dict()
+for i in arr:
+    # print(i)
+    my_dict[i] = my_dict.setdefault(i, 0) + 1
+
+maxi=0
+for key in sorted(my_dict.keys()):
+    
+
+print(my_dict)
