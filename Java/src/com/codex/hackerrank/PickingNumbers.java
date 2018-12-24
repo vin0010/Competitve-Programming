ccef6a2b1fecb98892e8594ac6f389ec70065af1
Vinoth G <vin10catchme@gmail.com>
2018-12-24 10:32:23 +0530

    Initial commit for https://www.hackerrank.com/challenges/picking-numbers/problem

---
+++ Java/src/com/codex/hackerrank/PickingNumbers.java
@@ -0,0 +1,17 @@
+package com.codex.hackerrank;
+/*
+ * https://www.hackerrank.com/challenges/picking-numbers/problem
+ */
+import java.util.Scanner;
+
+public class PickingNumbers {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		for (int t = 0; t < n; t++) {
+			arr[i] = scanner.nextInt();
+		}
+		scanner.close();
+	}
+}
