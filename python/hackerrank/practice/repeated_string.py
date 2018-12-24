9b80044bbfd503a66657cdc82556fe48b289f49a
Vinoth G <vin10catchme@gmail.com>
2018-12-24 14:55:52 +0530

    final solution for python/hackerrank/practice/repeated_string.py

--- python/hackerrank/practice/repeated_string.py
+++ python/hackerrank/practice/repeated_string.py
@@ -2,14 +2,28 @@
     https://www.hackerrank.com/challenges/repeated-string/problem
 """
 
-s = input()
-n = int(input())
+#!/bin/python3
 
-arr = list()
+import sys
 
-current_count: int = 0
-for i in range(len(s)):
-    if s[i] == 'a':
-        current_count += 1
-        arr[i] = current_count
-    print(s[i])
+
+s = input().strip()
+n = int(input().strip())
+countlist=[]
+acount=0
+for i in s:
+    if i=='a':
+        acount+=1
+        countlist.append(acount)
+    else:
+        countlist.append(acount)
+a = ((int)(n/len(s))*acount)
+b=(countlist[(n%len(s))-1]) if (n%len(s))>0 else 0
+# print("a: {0}".format(a))
+# print("b: {0}".format(b))
+sum=a+b;
+# print("((int)(n/len(s))*acount) : {0}".format(a))
+# print("(countlist[n%len(s)]) if (n%len(s))>0 else 0 : {0}".format(b))
+print(sum)
+# print(countlist)
+# print(((int)(n/len(s))*acount)+(countlist[n%len(s)]) if (n%len(s))>0 else 0) why its zero?
