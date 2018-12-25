898ba53ec88ade8a086de5f46e54b9d8cbd40acf
Vinoth G <vin10catchme@gmail.com>
2018-12-25 22:48:41 +0530

    Refactored code

--- python/hackerrank/practice/circular_rotation.py
+++ python/hackerrank/practice/circular_rotation.py
@@ -2,7 +2,6 @@
     https://www.hackerrank.com/challenges/circular-array-rotation/problem
 """
 
-
 def get_index(i, shift, l):
     if i == 0:
         return l - shift
@@ -13,18 +12,9 @@ def get_index(i, shift, l):
         return l + temp
 
 
-# def getIndex(i, r):
-#     return
-
-# print(get_index(0, 6, 8))
-# exit(0)
 n, rotations, queries = map(int, input().split())
 arr = list(map(int, input().split()))
-# k = n % rotations if n > rotations else rotations % n
 k = rotations % n
-# print(arr)
-
-# print("k:", k)
 while queries:
     index = int(input())
     # print("res:", abs(index - k))
